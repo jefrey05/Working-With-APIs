0195be29fdb506f254c829e54eb39cb5c1f762f7
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-15 15:08:51 -0400

    added javascript to dnd spell data  app

--- Dungeons-&-Dragons-SpellData/js/main.js
+++ Dungeons-&-Dragons-SpellData/js/main.js
@@ -5,19 +5,19 @@ function getFetch() {
   let choice = document.querySelector("input").value;
   let url = `https://www.dnd5eapi.co/api/spells/${choice}`;
   //document.querySelector('ul').removeChild('li')
-  let element= document.querySelector('ul');
-  while(element.firstChild){
-      element.removeChild(element.firstChild)
+  let element = document.querySelector("ul");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
   }
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data);
-    
+
       console.log(data.subclasses);
       document.querySelector("h2").innerText = data.name;
       document.querySelector("h3").innerText = data.classes[0].name;
-      document.querySelector('p').innerText = data.desc[0];
+      document.querySelector("p").innerText = data.desc[0];
       data.subclasses.forEach((obj) => {
         console.log(obj.name);
         //create an li
@@ -31,6 +31,5 @@ function getFetch() {
     .catch((err) => {
       console.log(`error ${err}`);
       alert("Enter a correct spell");
-      
     });
 }
