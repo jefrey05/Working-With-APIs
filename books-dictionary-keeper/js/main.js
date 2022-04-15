5b871ecbdf60d6a28c5419ba43cc57ed0007574d
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-15 16:39:54 -0400

    added javascript to books read app

--- books-dictionary-keeper/js/main.js
+++ books-dictionary-keeper/js/main.js
@@ -1,25 +1,37 @@
 //Example fetch using pokemonapi.co
 document.querySelector("button").addEventListener("click", getFetch);
-document.querySelector("h2").innerText = localStorage.getItem("books");
+//document.querySelector("h2").innerText = localStorage.getItem("books");
+for (const [key, value] of Object.entries(localStorage)) {
+  createRow(key,value)
+}
 function getFetch() {
   const choice = document.querySelector("input").value;
-  console.log(choice);
+  //console.log(choice);
   const url = `https://openlibrary.org/isbn/${choice}.json`;
-  9780140328721;
+  
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data.title);
+     // console.log(data.title);
       //put title into localStorage
-      if (!localStorage.getItem("books")) {
-        localStorage.setItem("books", data.title);
+      if (!localStorage.getItem(choice)) {
+        localStorage.setItem(choice, data.title);
+        createRow(choice,localStorage.getItem(choice));
+        console.log(`${choice} ${data.title}`)
       } else {
-        let books = localStorage.getItem("books") + " ; " + data.title;
-        localStorage.setItem("books", books);
+        alert("You have read this book already,Please enter another")
+        //createRow(choice,localStorage.getItem(choice));
       }
-      document.querySelector("h2").innerText = localStorage.getItem("books");
+      //document.querySelector("h2").innerText = localStorage.getItem(choice);
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
+}
+
+function createRow(choice,dataTitle){
+  console.log(choice);
+  console.log(dataTitle)
+  var newRow=document.querySelector('table').insertRow();
+  newRow.innerHTML = `<td>${choice}</td><td>${dataTitle}</td>`;
 }
