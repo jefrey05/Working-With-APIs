dfdea54278f16191a75aa3b464fe217d7988cd04
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-19 17:37:00 -0400

    made fixes on browser refresh get image from local storage

--- pick-random-meme/js/main.js
+++ pick-random-meme/js/main.js
@@ -1,5 +1,8 @@
 document.querySelector("button").addEventListener("click", getMeme);
-
+if(!localStorage.getItem('current')){
+  localStorage.setItem('current','');
+}
+document.querySelector("img").src = localStorage.getItem('current');
 function getMeme() {
   fetch("https://api.imgflip.com/get_memes")
     .then((res) => res.json()) // parse response as JSON
@@ -7,7 +10,6 @@ function getMeme() {
       console.log(data.data.memes);
       let rand = Math.floor(Math.random() * 100);
       document.querySelector("img").src = data.data.memes[rand].url;
-      document.querySelector("img").width = "400";
       document.querySelector("img").height = "400";
       document.querySelector("h2").classList.remove("hidden");
       document.querySelector("h2").innerText = data.data.memes[rand].name;
