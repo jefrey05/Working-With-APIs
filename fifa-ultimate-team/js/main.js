69b4dd8b49ef9ae87d9de1ad21e85627c730017b
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-20 15:49:59 -0400

    added html to fifa ultimate team

--- fifa-ultimate-team/js/main.js
+++ fifa-ultimate-team/js/main.js
@@ -1,1 +1,15 @@
+document.querySelector('button').addEventListener('click',getPlayer);
 
+function getPlayer(){
+
+    fetch("https://www.easports.com/fifa/ultimate-team/api/fut/item")
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+  
+
+}
