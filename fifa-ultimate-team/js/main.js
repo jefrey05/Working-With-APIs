c016643d083e9f9384789604d42558aad06a3a72
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-04-20 16:35:27 -0400

    added javascript to league app

--- fifa-ultimate-team/js/main.js
+++ fifa-ultimate-team/js/main.js
@@ -2,14 +2,29 @@ document.querySelector('button').addEventListener('click',getPlayer);
 
 function getPlayer(){
 
-    fetch("https://www.easports.com/fifa/ultimate-team/api/fut/item")
+    fetch("https://api-football-standings.azharimm.site/leagues")
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-      console.log(data)
+      console.log(data);
+      data.data.forEach(el=>{
+        createRow(el.id,el.name)
+        document.querySelector('#' + el.id).addEventListener('click',getStandings)
+      })
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
   
 
+}
+
+function createRow(choice, dataTitle) {
+  console.log(choice);
+  console.log(dataTitle);
+  var newRow = document.querySelector("table").insertRow();
+  newRow.innerHTML = `<td id="${choice}">${choice}</td><td>${dataTitle}</td>`;
+}
+
+function getStandings(){
+  console.log('this works')
 }
